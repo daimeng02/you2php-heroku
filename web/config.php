02f5dc1a0a8f527337f60fc21eb2b4e87e9d0243
daimeng02 <1977527202@QQ.com>
2018-03-31 19:00:08 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyDS5HkqOqFrkAvdUPt7ElX6NRfgPYooIms');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', 'u2b');
+define('TITLENAME', 'u2b');
+define('EN2DEKEY', 'HPERKEHF2b');
+define('EMAIL', '233@qq.com');
 ?>
